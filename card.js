5671a873a08b976b0a4d39ae2dba15c53bf016e9
Elga <elga.mancini@gmail.com>
2022-11-27 16:04:42 +0100

    finish first section cards

--- card.js
+++ card.js
@@ -9,19 +9,56 @@ const cardContent = [
       href: "#",
       cardImg: "/card_components/itt-hero-large-keyart-16x9-xl.jpg.adapt.crop16x9.431p.jpg",
       redTitle: "It Takes Two",
-      date: "4-nov-2022",
-      title: "It Takes Two è ora disponibile su Nintendo Switch<sup>tm</sup>",
-      text:"Immergiti subito nella più pazza avventura cooperativa di SWITCH<sup>tm</sup>",
+      date: "4 Nov 2022",
+      title: "It Takes Two è ora disponibile su Nintendo Switch<sup>TM</sup>",
+      text:"Immergiti subito nella più pazza avventura cooperativa di SWITCH<sup>TM</sup>",
     },
     {
         id: 1,
         href: "#",
         cardImg: "/card_components/marvel-x-ea-featured-image-16x9.jpg.adapt.crop16x9.431p.jpg",
-        redTitle: "It Takes Two",
-        date: "4-nov-2022",
-        title: "It Takes Two è ora disponibile su Nintendo Switch<sup>tm</sup>",
-        text:"Immergiti subito nella più pazza avventura cooperativa di SWITCH<sup>tm</sup>",
-      }]
+        redTitle: "Electronic Arts Inc.",
+        date: "31 Ott 2022",
+        title: "EA e MARVEL ENTERTAINMENT annunciano una collaborazione su più titoli",
+        text:"EA e MARVEL ENTERTAINMENT annunciano una collaburazione su più titoli per lo sviluppo di giochi d'azione e avventura.",
+      },
+      {
+        id: 2,
+        href: "#",
+        cardImg: "/card_components/ts4-play-sims-for-free-new-plumbob.png.adapt.crop16x9.431p.png",
+        redTitle: "Electronic Arts Inc.",
+        date: "18 Ott 2022",
+        title: "Giocate a The Sims 4 gratis a partire dal 18 ottobre",
+        text:"Ulteriori aggiornamenti a seguire durante uno streaming speciale: 'Behind The Sims Summit'",
+      },
+      {
+        id: 3,
+        href: "#",
+        cardImg: "/card_components/eacom-brand-featured-image-16x9.png.adapt.crop16x9.431p.png",
+        redTitle: "Electronic Arts Inc.",
+        date: "6 Ott 2022",
+        title: "Un aggiornamento per i nostri giocatori Mac sull'EA app per Windows",
+        text:"Forse avrai letto qualche notizia sulla nuova EA app per Windows, che presto diventerà il principale punto di riferimento per i giochi PC di EA, andando a sostituire Origin. Sappiamo che ti interessa sapere cosa questo comporti per te e per i tuoi amici che giocano su Origin per Mac.",
+      },
+      {
+        id: 4,
+        href: "#",
+        cardImg: "/card_components/ts4-play-sims-for-free-new-plumbob.png.adapt.crop16x9.431p.png",
+        redTitle: "Electronic Arts Inc.",
+        date: "18 Ott 2022",
+        title: "Tutto ciò che è stato annunciato durante l'evento 'Behind The Sims Summit",
+        text:"Uno sguardo al passato, al presente e al radioso futuro di The Sims",
+      },
+      {
+        id: 5,
+        href: "#",
+        cardImg: "/card_components/ea-app-featured-image-16x9.jpg.adapt.crop16x9.431p.jpg",
+        redTitle: "Electronic Arts Inc.",
+        date: "18 Ott 2022",
+        title: "Giocate a The Sims 4 gratis a partire dal 18 ottobre",
+        text:"Siamo lieti di annunciare che l'EA app ha ufficialmente abbandonato la fase open beta e presto sostituirà Origin in qualità di piattaforma primaria per PC. L'EA app è il client PC più veloce e leggero di sempre. Con il nuovo design semplificato, potrai trovare facilmente i giochi e i contenuti che stai cercando e scoprire i tuoi nuovi giochi preferiti.",
+      },
+    ]
 
     function newCards(row, item, content) {
         for (let i = 0; i < content.length; i++) {
@@ -35,8 +72,8 @@ const cardContent = [
                   <img src=${cardImg} class="card-img-top" alt="${title}">
                   <div class="card-body p-4">
                     <div class="d-flex flex-column align-items-center flex-md-row align-items-md-start mb-2">
-                      <span class="me-md-3"; style="color:#ff4747;font-weight:bold;">${redTitle}</span>
-                      <span style="color:#161616;">${date}</span>
+                      <span class="me-md-3"; style="color:#ff4747;font-weight:bold; font-size:.8rem">${redTitle}</span>
+                      <span style="color:#161616;font-size:.8rem">${date}</span>
                     </div>
                     <h3 class="h5 title text-center text-md-start mb-2">${title}</h3>
                     <p class="card-text text-dark">${text}</p>
