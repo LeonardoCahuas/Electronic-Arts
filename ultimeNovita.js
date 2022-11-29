08be8d3eeced30b13de323e83763e4bcbaf6f618
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-11-29 17:00:10 +0000

    CSS adjustments, button-links animation

--- ultimeNovita.js
+++
@@ -1,9 +0,0 @@
-const notizie = document.getElementById("nav-notizie-tab");
-
-notizie.addEventListener("click", myFn);
-
-function myFn(){
-    let notiziePar = notizie.firstElementChild;
-        notiziePar.style.borderBottom = "2px solid red";
-}
-
